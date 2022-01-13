64cf7732494ddfdf71b3078ba1fdea7b0c872a88
Tim Lebedev <timlebedev2@gmail.com>
2022-01-13 18:09:26 +0300

    some_fix

--- src/Images.js
+++ src/Images.js
@@ -1,12 +1,12 @@
-import Image01 from './memes/01.jpg';
-import Image02 from './memes/02.jpg';
-import Image03 from './memes/03.jpg';
-import Image04 from './memes/04.jpg';
-import Image05 from './memes/05.jpg';
-import Image06 from './memes/06.jpg';
-import Image07 from './memes/07.jpg';
-import Image08 from './memes/08.jpg';
+import Image01 from './images/01.jpg';
+import Image02 from './images/02.jpg';
+import Image03 from './images/03.jpg';
+import Image04 from './images/04.jpg';
+import Image05 from './images/05.jpg';
+import Image06 from './images/06.jpg';
+import Image07 from './images/07.jpg';
+import Image08 from './images/08.jpg';
 
 
 const Images = [Image01, Image02, Image03, Image04, Image05, Image06, Image07, Image08];
-export default Images;
+export default Images
